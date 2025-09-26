3f45f2a46b3bc405810b13c7b1544285c9d1a998
Your Name <you@example.com>
2025-09-26 17:09:51 +0300

    fix product page layout

--- src/pages/product/ui/product-media.vue
+++ src/pages/product/ui/product-media.vue
@@ -19,52 +19,54 @@ function getDefaultUrl(image?: StoreProductImage) {
 const mainImage = ref(product?.value?.images?.[0]);
 </script>
 <template>
-  <div class="lg:flex hidden gap-4 w-full">
-    <div class="flex flex-col gap-3">
-      <img
-        v-for="image of product?.images"
-        :src="getThumbnailUrl(image)"
-        alt="Product Image"
-        class="max-w-[5.75rem] cursor-pointer aspect-[23/28] object-cover"
-        @click="mainImage = image"
-      />
-    </div>
-    <div>
-      <img
-        :src="getDefaultUrl(mainImage)"
-        alt="Product Image"
-        class="object-cover"
-      />
-    </div>
-  </div>
-
   <div>
-    <ClientOnly>
-      <swiper-container
-        :pagination="{
-          el: '.product-card-pagination',
-          type: 'bullets',
-        }"
-        ref="containerRef"
-        class="aspect-[15/18]"
-      >
-        <swiper-slide
+    <div class="lg:flex hidden gap-4 w-full">
+      <div class="flex flex-col gap-3">
+        <img
           v-for="image of product?.images"
-          :key="image.id"
-          class="size-full"
+          :src="getThumbnailUrl(image)"
+          alt="Product Image"
+          class="max-w-[5.75rem] cursor-pointer aspect-[23/28] object-cover"
+          @click="mainImage = image"
+        />
+      </div>
+      <div>
+        <img
+          :src="getDefaultUrl(mainImage)"
+          alt="Product Image"
+          class="object-cover"
+        />
+      </div>
+    </div>
+
+    <div class="lg:hidden">
+      <ClientOnly>
+        <swiper-container
+          :pagination="{
+            el: '.product-card-pagination',
+            type: 'bullets',
+          }"
+          ref="containerRef"
+          class="aspect-[15/18]"
         >
-          <NuxtImg
-            class="object-cover object-center size-full"
-            :src="image.url"
-          />
-        </swiper-slide>
-        <div slot="container-end">
-          <div
-            class="product-card-pagination z-10 absolute w-full gap-1 bottom-2 px-2 flex"
-          ></div>
-        </div>
-      </swiper-container>
-    </ClientOnly>
+          <swiper-slide
+            v-for="image of product?.images"
+            :key="image.id"
+            class="size-full"
+          >
+            <NuxtImg
+              class="object-cover object-center size-full"
+              :src="image.url"
+            />
+          </swiper-slide>
+          <div slot="container-end">
+            <div
+              class="product-card-pagination z-10 absolute w-full gap-1 bottom-2 px-2 flex"
+            ></div>
+          </div>
+        </swiper-container>
+      </ClientOnly>
+    </div>
   </div>
 </template>
 <style lang="css">
