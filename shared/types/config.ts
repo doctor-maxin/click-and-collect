1248da3c7927976afe72edf964b4ce7d2759907a
Your Name <you@example.com>
2025-10-01 17:05:38 +0300

    add cookie-banner

--- shared/types/config.ts
+++ shared/types/config.ts
@@ -1,10 +1,14 @@
 export interface IGlobalConfig {
-  data: {
+  config: {
     id: number;
     documentId: string;
     address: string;
     email: string;
     phone: string;
     yandexMapKey: string;
+    cookieBanner: {
+      header: string;
+      content: string;
+    };
   };
 }
