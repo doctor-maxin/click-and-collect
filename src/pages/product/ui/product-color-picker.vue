40057d7304ac8f102c8c260ed68603b0422fac84
Your Name <you@example.com>
2025-10-16 17:36:23 +0300

    Feat sort option values by sequance

--- src/pages/product/ui/product-color-picker.vue
+++ src/pages/product/ui/product-color-picker.vue
@@ -2,39 +2,43 @@
 import { useProductStore } from "../lib/product-store";
 
 const productStore = useProductStore();
-const { product, color } = storeToRefs(productStore);
+const { product, color, variant } = storeToRefs(productStore);
 
 const colorImage = (color: string) => {
-  const image = product.value?.images?.find(
-    (i) => i.metadata?.color?.toLowerCase() === color?.toLowerCase(),
-  );
-  console.log(image);
-  return image?.url ?? "/not_found.png";
+    const image = product.value?.images?.find(
+        (i) => i.metadata?.color?.toLowerCase() === color?.toLowerCase(),
+    );
+    console.log(image);
+    return image?.url ?? "/not_found.png";
 };
 </script>
 <template>
-  <div class="flex my-4 lg:my-9 flex-col gap-5 lg:gap-4">
-    <span
-      >Цвет: <span class="capitalize">{{ color?.value }}</span></span
-    >
-    <div class="flex overflow-x-auto gap-3 w-full">
-      <article
-        v-for="item of product?.options?.find((o) => o.title === 'color')
-          ?.values"
-        :key="item.id"
-        class="cursor-pointer border-2"
-        @click="productStore.selectColor(item)"
-        :class="{
-          ' border-blue': color?.id === item?.id,
-          ' border-gray': color?.id !== item?.id,
-        }"
-      >
-        <img
-          class="aspect-[23/28] object-cover max-w-[5.75rem]"
-          :src="colorImage(item.value)"
-          :alt="`Изображение товара ${item.value}`"
-        />
-      </article>
+    <div class="flex my-4 lg:my-9 flex-col gap-5 lg:gap-4">
+        <span
+            >Цвет:
+            <span class="capitalize">{{
+                variant?.metadata?.colorTranslation ?? color?.value
+            }}</span></span
+        >
+        <div class="flex overflow-x-auto gap-3 w-full">
+            <article
+                v-for="item of product?.options?.find(
+                    (o) => o.title === 'color',
+                )?.values"
+                :key="item.id"
+                class="cursor-pointer border-2"
+                @click="productStore.selectColor(item)"
+                :class="{
+                    ' border-blue': color?.id === item?.id,
+                    ' border-gray': color?.id !== item?.id,
+                }"
+            >
+                <img
+                    class="aspect-[23/28] object-cover max-w-[5.75rem]"
+                    :src="colorImage(item.value)"
+                    :alt="`Изображение товара ${item.value}`"
+                />
+            </article>
+        </div>
     </div>
-  </div>
 </template>
--- src/pages/product/ui/product-size-picker.vue
+++ src/pages/product/ui/product-size-picker.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
 import { useProductStore } from "../lib/product-store";
+import { sortSizeOptions } from "~/shared/lib/utils/sort-size-options";
 import type { StoreProductVariant } from "@medusajs/types";
 
 const productStore = useProductStore();
 const { size, product } = storeToRefs(productStore);
+
+const options = computed(() => {
+    const list =
+        product.value?.options?.find((o) => o.title === "size")?.values ?? [];
+    if (!list || !product.value?.variants) return [];
+
+    return sortSizeOptions(list, product.value?.variants);
+});
 </script>
 <template>
-  <section class="flex flex-col gap-4 my-4 lg:my-9">
-    <span>Размеры</span>
-    <div
-      class="flex text-base leading-5 gap-y-4 gap-8 w-full max-w-full flex-wrap"
-    >
-      <label
-        v-for="item of product?.options?.find((o) => o.title === 'size')
-          ?.values"
-        :key="item.id"
-        class="cursor-pointer border relative py-1 px-2"
-        :class="{
-          '  border-blue ': size?.id === item?.id,
-          ' border-transparent': size?.id !== item?.id,
-        }"
-        @click="productStore.selectSize(item)"
-      >
-        {{ item.value }}
-      </label>
-    </div>
-  </section>
+    <section class="flex flex-col gap-4 my-4 lg:my-9">
+        <span>Размеры</span>
+        <div
+            class="flex text-base leading-5 gap-y-4 gap-8 w-full max-w-full flex-wrap"
+        >
+            <label
+                v-for="item of options"
+                :key="item.id"
+                class="cursor-pointer border relative py-1 px-2"
+                :class="{
+                    '  border-blue ': size?.id === item?.id,
+                    ' border-transparent': size?.id !== item?.id,
+                }"
+                @click="productStore.selectSize(item)"
+            >
+                {{ item.value }}
+            </label>
+        </div>
+    </section>
 </template>
