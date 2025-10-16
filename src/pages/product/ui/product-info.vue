3d662fe16e13897bac16fbb01562f37d7d057a73
Your Name <you@example.com>
2025-10-16 13:12:05 +0300

    feat marketplace buttons

--- src/pages/product/ui/product-info.vue
+++ src/pages/product/ui/product-info.vue
@@ -11,6 +11,26 @@ const sku = computed(() =>
     product?.value?.variants?.[0]?.sku?.split("-").splice(0, 3).join("-"),
 );
 
+const marketplaces = computed(
+    () =>
+        product?.value?.metadata?.marketplaces as {
+            link: string;
+            provider: string;
+        }[],
+);
+
+const getMarketplaceLink = (provider: string) => {
+    let names: Record<string, string> = {
+        WB: "Купить на Wildberries",
+    };
+
+    return names[provider] || "Купить на " + provider;
+};
+
+const copyLink = (link: string) => {
+    navigator.clipboard.writeText(link);
+};
+
 watch(
     variant,
     (v) => {
@@ -57,7 +77,41 @@ watch(
         >
         <ProductColorPicker />
         <ProductSizePicker />
-        <span class="text-[1.25rem] leading-6 my-0"
+        <div
+            v-if="marketplaces?.length"
+            class="my-6 gap-6 w-full flex flex-col"
+        >
+            <span class="text-gray text-base leading-5"
+                >Данная цена может отличаться от цены в магазинах и на
+                маркетплейсах</span
+            >
+            <div
+                type="button"
+                v-for="item of marketplaces"
+                :href="item.link"
+                role="link"
+                class="flex w-full max-w-[18.25rem]"
+            >
+                <a
+                    :href="item.link"
+                    class="flex cursor-pointer h-12"
+                    target="_blank"
+                >
+                    <span
+                        class="flex px-10 items-center justify-center text-center border border-blue rounded-l-lg"
+                        >{{ getMarketplaceLink(item.provider) }}</span
+                    >
+                </a>
+                <button
+                    type="button"
+                    class="h-12 cursor-pointer border-y border-blue border-r rounded-r px-2.5"
+                    @click="copyLink(item.link)"
+                >
+                    <SvgoCopy class="!mb-0 text-2xl" filled />
+                </button>
+            </div>
+        </div>
+        <span v-else class="text-[1.25rem] leading-6 my-0"
             >Товар доступен только в розничном магазине</span
         >
     </div>
