8570b2a62da1264c863dc98ac82755ef000e0e36
Your Name <you@example.com>
2025-11-10 13:31:00 +0300

    - Исправление хлебных крошек в категориях

--- src/pages/category/ui/category-breadcrumbs.vue
+++ src/pages/category/ui/category-breadcrumbs.vue
@@ -5,23 +5,40 @@ const { category } = defineProps<{
   category: StoreProductCategory;
 }>();
 
+const { data: product_categories } =
+  useNuxtData<StoreProductCategory[]>("categories");
+
 const breadcrumbs = computed(() => {
   const list = [];
   let c: StoreProductCategory | null = category;
-  while (c) {
-    if (!c.parent_category) break;
-    list.push({
-      path: `/catalog/` + c.handle,
-      label: `${c.name[0]?.toUpperCase()}${c.name.slice(1).toLowerCase()}`,
-    });
-    c = c.parent_category;
-  }
+
+  if (!product_categories.value) return list;
+
+  // @ts-ignore
+  const mpath = c.mpath.split(".");
+  let tmpCat: StoreProductCategory | undefined;
+
   list.push({
     path: `/`,
     label: "Главная",
   });
 
-  return list.reverse();
+  for (const id of mpath) {
+    if (tmpCat) {
+      tmpCat = tmpCat.category_children.find((child) => child.id === id);
+    } else {
+      tmpCat = product_categories.value.find((c) => c.id === id);
+    }
+    if (!tmpCat) continue;
+
+    if (tmpCat.name === "SINSAY") continue;
+    list.push({
+      path: `/catalog/` + tmpCat.handle,
+      label: `${tmpCat.name[0]?.toUpperCase()}${tmpCat.name.slice(1).toLowerCase()}`,
+    });
+  }
+
+  return list;
 });
 </script>
 <template>
