9b20d6b93872f6b09dede27b1cc1485afc0c3040
Mukhriddin <doctor-maxin@yandex.ru>
2026-05-29 18:56:22 +0300

    - Disable swr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,10 +22,10 @@ export default defineNuxtConfig({
     ],
     $production: {
         routeRules: {
-            "/": { swr: 5 },
-            "/product/**": { swr: 5 },
-            "/catalog/**": { swr: 5 },
-            "/pages/**": { swr: 300 },
+            // "/": { swr: 5 },
+            // "/product/**": { swr: 5 },
+            // "/catalog/**": { swr: 5 },
+            // "/pages/**": { swr: 300 },
         },
     },
     site: {
