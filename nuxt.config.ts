4ff08b7cca31d70563c78e4622d6dc7eb9c55798
Mukhriddin <doctor-maxin@yandex.ru>
2026-06-02 13:19:17 +0300

    - Добавление яндекс метрики

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,6 +71,24 @@ export default defineNuxtConfig({
             htmlAttrs: {
                 lang: "ru",
             },
+            script: [
+                {
+                    innerHTML: `
+                    (function(m,e,t,r,i,k,a){
+                        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109560870', 'ym');
+
+                    ym(109560870, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`,
+                },
+            ],
+            noscript: [
+                {
+                    innerHTML: `<div><img src="https://mc.yandex.ru/watch/109560870" style="position:absolute; left:-9999px;" alt="" /></div>`,
+                },
+            ],
         },
     },
     dir: {
